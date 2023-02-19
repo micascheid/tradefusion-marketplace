69d5270f1bed93d25153e419f89d38517b543609
Mica Linscheid <micascheid@gmail.com>
2023-02-19 08:03:03 -0300

    little commit, putting landing page on back burner while I get mvp ready to show quants

--- src/pages/extra-pages/SamplePage.js
+++ src/pages/extra-pages/SamplePage.js
@@ -1,5 +1,5 @@
 // material-ui
-import { Typography } from '@mui/material';
+import {Divider, Typography} from '@mui/material';
 
 // project import
 import MainCard from 'components/MainCard';
@@ -7,13 +7,14 @@ import MainCard from 'components/MainCard';
 // ==============================|| SAMPLE PAGE ||============================== //
 
 const SamplePage = () => (
-    <MainCard title="Sample Card">
-        <Typography variant="body2">
-            Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif
-            ad minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in
-            reprehended in voltage veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean cuspidate non president, sunk in culpa
-            qui officiate descent molls anim id est labours.
-        </Typography>
+    <MainCard title="Support">
+      <Typography variant="body2">
+          micalinscheid@tradefusion.io
+      </Typography>
+      <Divider sx={{width: '100%'}}/>
+      <Typography variant="body2">
+        Reach out...
+      </Typography>
     </MainCard>
 );
 
